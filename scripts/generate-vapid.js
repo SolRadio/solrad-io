e4908b61061466836843f42743b8178e4a3c3c07
Solrad.io <support@solrad.io>
2026-03-03 13:28:15 -0500

    Delete scripts/generate-vapid.js

--- scripts/generate-vapid.js
+++
@@ -1,4 +0,0 @@
-import webPush from "web-push"
-const keys = webPush.generateVAPIDKeys()
-console.log("NEXT_PUBLIC_VAPID_PUBLIC_KEY=" + keys.publicKey)
-console.log("VAPID_PRIVATE_KEY=" + keys.privateKey)
